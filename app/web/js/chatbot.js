8d6484598fdc3559763ffac59d286abcf8750d20
César <pedrocesaracc@gmail.com>
2024-01-11 04:21:21 +0000

    update

--- app/web/js/chatbot.js
+++ app/web/js/chatbot.js
@@ -12,7 +12,7 @@ const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
     chatLi.classList.add("chat", `${className}`);
-    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
+    let chatContent = className === "outgoing" ? `<p></p>` : `<span class="fa fa-info"></span><p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi; // return chat <li> element
